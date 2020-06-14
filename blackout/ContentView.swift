ebc342d4665d20223bd48ba684b4a4ed5f1751f5
shmam <samuel.d.crochet@gmail.com>
2020-06-14 13:59:36 -0500

    Initial Commit

---
+++ blackout/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  blackout
+//
+//  Created by Sam Crochet on 6/14/20.
+//  Copyright © 2020 Sam Crochet. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
